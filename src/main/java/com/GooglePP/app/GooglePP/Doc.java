b47c6260a938df4dfca7715a2279ef7ea318c57d
Tuan Pham Minh <gearhunter@gmx.de>
2014-12-08 21:26:38 +0100

    doc class documented

--- src/main/java/com/GooglePP/app/GooglePP/Doc.java
+++ src/main/java/com/GooglePP/app/GooglePP/Doc.java
@@ -9,7 +9,19 @@ import org.apache.lucene.document.IntField;
 import org.apache.lucene.document.LongField;
 import org.apache.lucene.document.StringField;
 
+/**
+ *	a class to store information about a document
+ *
+ */
 public class Doc {
+	/**
+	 * constructor, which takes the id, date, title, and the text of a document
+	 * 
+	 * @param id an unique id, which is assigned to the document
+	 * @param date the date this document got published
+	 * @param title the title of the document
+	 * @param text the text belonging to the document
+	 */
 	public Doc(int id, Date date, String title, String text) {
 		m_id = id;
 		m_date = date;
@@ -17,19 +29,38 @@ public class Doc {
 		m_text = text;
 	}
 	
+	/**
+	 * getter for the id
+	 * @return the unique id of this document
+	 */
 	public int getId() {
 		return m_id;
 	}
+	/**
+	 * getter for the date
+	 * @return the date this document got published
+	 */
 	public Date getDate() {
 		return m_date;
 	}
+	/**
+	 * getter for the title
+	 * @return the title of this document
+	 */
 	public String getTitle() {
 		return m_title;
 	}
+	/**
+	 * getter for the text
+	 * @return the text of this document
+	 */
 	public String getText() {
 		return m_text;
 	}
-	
+	/**
+	 * a conversion method to generate a org.w3c.dom.Document out of the instance
+	 * @return an org.w3c.dom.Document with the same information 
+	 */
 	public Document toDocument(){
 		Document d = new Document();
 		
@@ -47,9 +78,21 @@ public class Doc {
 		
 		return d;
 	}
-
+	
+	/**
+	 * the unique id of this document
+	 */
 	private int m_id;
+	/**
+	 * the date this document got published
+	 */
 	private Date m_date;
+	/**
+	 * the title of this document
+	 */
 	private String m_title;
+	/**
+	 * the text belonging to this document
+	 */
 	private String m_text;
 }
